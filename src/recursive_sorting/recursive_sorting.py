ac9a3440f4e62c790bea5ade96e86af18134b12d
Justin <showin@live.com>
2020-01-22 12:28:28 -0700

    update recursive_sorting

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,15 +1,63 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+    
+    # Add lengths of arrA and arrB to get length needed for new array
+    elements = len(arrA) + len(arrB)
+    # Create new array of zeros with length of elements
     merged_arr = [0] * elements
-    # TO-DO
+    # Create pointer variables
+    i = j = k = 0
+    while i < len(arrA) and j < len(arrB):
+        # Check if value at index i in arrA is smaller or equal to
+        # the value at the corresponding index of arrB
+        if arrA[i] < arrB[j]:
+            # If True, add value at index i in arrA to merged_arr at
+            # index k
+            merged_arr[k] = arrA[i]
+            # Increment i since we used that value already
+            i += 1
+        else:
+            # Add value at index j in arrB to merged_arr at index k,
+            # otherwise.
+            merged_arr[k] = arrB[j]
+            # Increment j since we used that value alread
+            j += 1
+        # Increment k since we filled that slot in merged_arr
+        k += 1
+    # If we run out of elemnts in arrA or arrB, we need to
+    # continue adding the elements from the other array to
+    # merged_arr and increment accordingly.
+    while i < len(arrA):
+        merged_arr[k] = arrA[i]
+        i += 1
+        k += 1
+    while j < len(arrB):
+        merged_arr[k] = arrB[j]
+        j += 1
+        k += 1
     
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    # Base case
+    if len(arr) <= 1:
+        return arr
+
+    else:
+        # Cut array in half
+        middle = len(arr) // 2
+
+        # Create 2 new arrays to be passed recursively to the merge() function
+        # Recursively call the merge_sort() function on left and right
+        left = merge_sort(arr[:middle])
+        right = merge_sort(arr[middle:])
+
+        # Merge the two arrays
+        arr = merge(left, right)
+
+        return arr
 
     return arr
 
